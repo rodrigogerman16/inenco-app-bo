284f032d2f8fc2d75a6b799ae8356fd8a6b9cc18
Rodrigo <rodrigoezequielgerman@gmail.com>
2025-11-04 14:12:29 +0100

    fix: next config warnings

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
+import path from "path"
+import { fileURLToPath } from "url"
+
 /** @type {import('next').NextConfig} */
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const nextConfig = {
+  outputFileTracingRoot: path.join(__dirname),
   eslint: {
     ignoreDuringBuilds: true,
   },
